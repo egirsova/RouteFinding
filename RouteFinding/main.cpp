08d46a767d6fb3c5cb72b8c099045e5a56d97b5e
Liza Girsova <Liza Girsova@fondowt-1.student.lawrence.edu>
2014-10-04 18:10:41 -0500

    Initial Commit

---
+++ RouteFinding/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RouteFinding
+//
+//  Created by Liza Girsova on 10/4/14.
+//  Copyright (c) 2014 CMSC470. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
